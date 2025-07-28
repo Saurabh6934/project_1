6608058c42ff80b3600db397ac844b421f8b33eb
KUMAR PATEL <saurabh.kumar-patel@capgemini.com>
2025-07-28 22:14:43 +0530

    updated code

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "strg1" {
+  name                     = var.strg_name
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
+
+
+
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features{}
+  subscription_id = "b2744a0d-e7f7-4cea-af52-689c2e4a2c69"
+}
